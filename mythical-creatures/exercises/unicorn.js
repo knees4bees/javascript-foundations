10d7c54957caea064fb250052c4386ef95ee5023
Katie Ballentine <knees4bees@gmail.com>
2020-12-09 22:51:15 -0700

    Add unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,24 @@
 class Unicorn {
   constructor(name, color) {
+    console.log(color);
     this.name = name;
-    this.color = color;
+    if (color === undefined) {
+      this.color = "white";
+    } else {
+      this.color = color;
+    }
   }
+
+  isWhite() {
+    if (this.color !== "white") {
+      return false;
+    }
+  }
+
+  says(phrase) {
+    return `**;* ${phrase} *;**`;
+  }
+
 }
 
 module.exports = Unicorn;
