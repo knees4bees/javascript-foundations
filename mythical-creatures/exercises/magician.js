63e9821e7e309779981740a6d67dc29219769dda
Katie Ballentine <knees4bees@gmail.com>
2020-12-15 22:42:10 -0700

    Complete magician

---
+++ mythical-creatures/exercises/magician.js
@@ -0,0 +1,38 @@
+class Magician {
+  constructor(magician) {
+    this.name = `The Great ${magician.name}`;
+    this.assistant = magician.assistant;
+    if (magician.clothing) {
+      this.favoriteAccessory = magician.clothing;
+    } else {
+      this.favoriteAccessory = 'top hat';
+    }
+    this.confidencePercentage = 10;
+  }
+
+  performIncantation(magicWord) {
+    return `${magicWord.toUpperCase()}!`
+  }
+
+  performTrick() {
+    this.confidencePercentage += 10;
+
+    if (this.favoriteAccessory === 'top hat') {
+      return 'PULL RABBIT FROM TOP HAT';
+    } else {
+      return 'PULL DOVE FROM SLEEVE';
+    }
+  }
+
+  performShowStopper() {
+    if (this.confidencePercentage >= 100 && this.assistant) {
+      return 'WOW! The magician totally just sawed that person in half!';
+    } else {
+      return 'Oh no, this trick is not ready!';
+    }
+  }
+}
+
+
+
+module.exports = Magician;
