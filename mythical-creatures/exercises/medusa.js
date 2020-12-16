f138b1bf2a30c9b8dd1863073bb60bbdec5db626
Katie Ballentine <knees4bees@gmail.com>
2020-12-16 15:42:49 -0700

    Complete medusa

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -6,6 +6,21 @@ class Medusa {
     this.name = name;
     this.statues = [];
   }
+
+  gazeAtVictim(person) {
+    var newStatue = new Statue(person.name);
+
+    if (this.statues.length === 3) {
+      var rePerson = new Person(this.statues.shift().name);
+      rePerson.mood = 'relieved';
+    }
+
+    if (this.statues.length < 3) {
+      this.statues.push(newStatue);
+    }
+
+    return rePerson;
+  }
 }
 
 module.exports = Medusa;
